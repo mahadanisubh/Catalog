ffe27d6800161f8a2f3ae14e01b33c553247a0cd
Subha Mahadani <mahadanisubh@gmail.com>
2026-02-01 16:39:14 +0530

    Change CORS origin to production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://catalog-2q8w.onrender.com",
   credentials: true
 }))
 app.use("/", router);
